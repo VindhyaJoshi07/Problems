41344f53d2dbb5b95ee31fe2a4b61a41a4d941a0
Vindhya Kankurthi <vindyaj07@gmail.com>
2024-02-29 21:37:33 -0800

    Array Rotation

--- TwoPointers/rotateArray.js
+++ TwoPointers/rotateArray.js
@@ -1,15 +1,39 @@
-// function rotataArray(arr, numberOfShifts) {
+// function rotateArray(arr, numberOfShifts) {
 //     for(let i = 0; i< numberOfShifts; i++) {
 //         arr.unshift(arr.pop());
 //         console.log(`step ${i+1}:`, arr);
 //     }
 // }
 
+const numbers = [3,4,5,6,7,8,9];
+const key = 3;
 
-function rotataArray(arr, numberOfShifts) {
+function rotateArray(arr, numberOfShifts) {
     let deletedArray = arr.splice(arr.length - numberOfShifts); // [5,6,7]
     for(let i = 0; i< deletedArray; i++) {
         arr.splice(i, 0, deletedArray[i]);
         console.log(`step ${i+1}:`, arr);
     }
 }
+
+console.log("Final Output: ", rotateArray(numbers, key));
+
+
+let rotate = function(nums, k) {
+    nums.reverse();
+    k = k % nums.length;
+    let last = k - 1;
+    let i = 0;
+    while(i < last) {
+        [nums[i], nums[last]] = [nums[last], nums[i]];
+        last--;
+        i++;
+    }
+    last = nums.length - 1;
+    i = k;
+    while(i < last) {
+        [nums[i], nums[last]] = [nums[last], nums[i]];
+        last--;
+        i++;
+    }
+};
