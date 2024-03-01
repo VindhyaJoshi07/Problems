700c8a461ab9d3e075d768e4932cf808b780c10a
Vindhya Kankurthi <vindyaj07@gmail.com>
2024-02-29 21:53:16 -0800

    commit changes

--- TwoPointers/rotateArray.js
+++ TwoPointers/rotateArray.js
@@ -1,12 +1,17 @@
-// function rotateArray(arr, numberOfShifts) {
-//     for(let i = 0; i< numberOfShifts; i++) {
-//         arr.unshift(arr.pop());
-//         console.log(`step ${i+1}:`, arr);
-//     }
-// }
-
 const numbers = [3,4,5,6,7,8,9];
 const key = 3;
+
+function rotateArray(arr, numberOfShifts) {
+    for(let i = 0; i< numberOfShifts; i++) {
+        arr.unshift(arr.pop());
+        console.log(`step ${i+1}:`, arr);
+    }
+}
+console.log("Final Output: ", rotateArray(numbers, key));
+
+// ------------------------------------------------------------------------
+// const numbers = [3,4,5,6,7,8,9];
+// const key = 3;
 
 function rotateArray(arr, numberOfShifts) {
     let deletedArray = arr.splice(arr.length - numberOfShifts); // [5,6,7]
@@ -18,7 +23,7 @@ function rotateArray(arr, numberOfShifts) {
 
 console.log("Final Output: ", rotateArray(numbers, key));
 
-
+// --------------------------------------------------------------------------
 let rotate = function(nums, k) {
     nums.reverse();
     k = k % nums.length;
